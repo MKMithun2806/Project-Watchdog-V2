9b2cd625bc86c3ee9d71c7e2bf62433028e80e7c
Mithun <mmithunkrish@gmail.com>
2026-05-24 12:12:26 +0000

    fix: updated lambda func modes

--- Terraform/lambda_function.py
+++ Terraform/lambda_function.py
@@ -6,9 +6,9 @@ import os
 ec2 = boto3.client('ec2', region_name=os.environ['AWS_REGION'])
 
 MODES = {
-    'normal':  '',
-    'stealth': 'stealth',
-    'head':    'head',
+    'normal':  {'v_flags': '', 'n_flags': ''},
+    'stealth': {'v_flags': '--polite --slow', 'n_flags': '--stealth'},
+    'head':    {'v_flags': '--polite --quiet --headless', 'n_flags': ''},
 }
 
 AMI_ID        = os.environ['AMI_ID']
@@ -44,9 +44,14 @@ def lambda_handler(event, context):
     if mode not in MODES:
         return {'statusCode': 400, 'body': json.dumps({'error': f'invalid mode, pick: {list(MODES.keys())}'})}
 
+    v_flags = MODES[mode]['v_flags']
+    n_flags = MODES[mode]['n_flags']
+
     user_data = f"""#!/bin/bash
 export TARGET="{target}"
 export MODE="{mode}"
+export VULNMALPER_FLAGS="{v_flags}"
+export NETMALPER_FLAGS="{n_flags}"
 export EXPORT_JSON="{'true' if export_json else 'false'}"
 export SUPABASE_URL="{SUPABASE_URL}"
 export SUPABASE_KEY="{SUPABASE_KEY}"
